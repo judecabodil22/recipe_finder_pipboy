73e2e2ba5845ec8ff4778c3e321c8aea55ad111a
Justin <judecabodil22@gmail.com>
2025-11-27 09:32:25 +0800

    Implement Fallout Pip-Boy theme and add GitHub Pages deployment.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,24 +1,24 @@
 import react from 'react';
 
-export default function Footer () {
-    return ( <>
-           <div 
-                className="terminal-status-footer" 
-                style={{
-                    position: 'absolute', 
-                    bottom: '10px', 
-                    left: '20px', 
-                    right: '20px', 
-                    fontSize: '0.8em', 
-                    textAlign: 'center', 
-                    color: 'var(--fallout-shadow)' 
-                }}
-            >
-                {'> STATUS: ONLINE | VERSION 4.1 | DATA MOCKING ACTIVE <'}
-            </div>
-    
+export default function Footer() {
+    return (<>
+        <div
+            className="terminal-status-footer"
+            style={{
+                position: 'relative',
+                marginTop: '40px',
+                marginBottom: '10px',
+                width: '100%',
+                fontSize: '0.8em',
+                textAlign: 'center',
+                color: 'var(--fallout-shadow)'
+            }}
+        >
+            {'> STATUS: ONLINE | VERSION 4.1 | DATA MOCKING ACTIVE <'}
+        </div>
+
     </>
-           
-            );
-     
+
+    );
+
 }
